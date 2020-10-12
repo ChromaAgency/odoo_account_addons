b0286c275dc5fe26d2aeadb70ad97ac2e744d4a9
Federico Gobea <fgobea@flopsolutions.com>
2020-10-12 00:52:46 -0300

    [ADD] added changes *wow very explained*

--- account_payment_group/models/models.py
+++ account_payment_group/models/models.py
@@ -2,9 +2,17 @@
 from odoo import models, fields, api
 from odoo.fields import One2many, Many2many,Many2one,Date,Float,Char
 from odoo.api import depends
-class PaymentGroup(models.Model):
+from odoo.models import Model
+
+class AccountPayment(Model):
+    _inherit = 'account.payment'
+    
+    payment_group_id = Many2one('account.payment.group', string="Payment Group")
+class PaymentGroup(Model):
     _name = 'account.payment.group'
     _description = 'Groups different lines of account.payment and relates them with account.move lines (invoices, and other)'
+    _inherit = 'mail.thread'
+    _order = "payment_date desc"
 
     payment_lines_ids = One2many('account.payment','payment_group_id', string="Pagos")
     move_line_ids = Many2many('account.move',string="Movimientos imputados") 
@@ -19,8 +27,10 @@ class PaymentGroup(models.Model):
     def _compute_payments_total(self):
         for rec in self:
             rec.payments_total = sum(rec.payment_lines_ids.mapped('amount'))
-    
+
     @depends('payments_total')
-    def __compute_matched_amount(self)
+    def _compute_matched_amount(self):
         for rec in self:
             rec.matched_amount = rec.payments_total - sum(rec.move_line_ids.mapped('amount_residual'))
+            #TODO hacer esta parte 
+            rec.unmatched_amount = sum(rec.move_line_ids.mapped('amount_residual'))
