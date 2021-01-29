fcbb4242e4340409a3ce47ce2eb5e762c2f405cc
Federico Gobea <fgobea@flopsolutions.com>
2021-01-29 12:13:31 -0300

    [FIX] create bill button not working correctly

--- account_pricelist_selector/models/account_move.py
+++ account_pricelist_selector/models/account_move.py
@@ -5,7 +5,13 @@ from odoo.api import onchange,model
 class AccountMove(Model):
   _inherit = "account.move"
 
-  pricelist_id = Many2one('product.pricelist',default=1,string="Lista de precios",check_company=True,  # Unrequired company
+  @model
+  def _get_default_pricelist_id(self): 
+    if self.type in ['in_invoice','in_refund']:
+      return False
+    return self.partner_id.property_product_pricelist.id
+
+  pricelist_id = Many2one('product.pricelist',default=_get_default_pricelist_id,string="Lista de precios",check_company=True,  # Unrequired company
         readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
         help="If you change the pricelist, only newly added lines will be affected.")
@@ -24,11 +30,11 @@ class AccountMove(Model):
 
   @onchange('pricelist_id')
   def _onchange_pricelist_id(self):
-    self.ensure_one()
-    for line in self.invoice_line_ids:
-      if line.product_id:
-        line.price_unit = self.pricelist_id.get_product_price(line.product_id,line.quantity,self.partner_id)
-        line._onchange_price_subtotal()
-    
-    self._onchange_invoice_line_ids()
-    return
+    if self.pricelist_id:
+      self.ensure_one()
+      for line in self.invoice_line_ids:
+        if line.product_id:
+          line.price_unit = self.pricelist_id.get_product_price(line.product_id,line.quantity,self.partner_id)
+          line._onchange_price_subtotal()
+      
+      self._onchange_invoice_line_ids()
