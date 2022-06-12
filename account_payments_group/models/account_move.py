e62c99f998f85ad7313bc293f17944750e165ac2
Federico Gobea <65693362+fedegobea@users.noreply.github.com>
2022-06-12 19:17:13 -0300

    🔧 Refactored to solve issues

---
+++ account_payments_group/models/account_move.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.fields import Integer, One2many, Many2many, Many2one, Date, Float, Char, Text, Selection
+from odoo.api import depends, onchange, returns
+from odoo.models import Model
+from odoo.exceptions import UserError
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class AccountMove(Model):
+    _inherit = 'account.move'
+
+    payment_group_ids = Many2many('account.payment.group',string="Grupos de pagos", compute="_compute_payment_group_ids")
+    payment_group_count = Integer(string="Pagos", compute="_compute_payment_group_ids")
+
+    def open_payment_groups(self):
+        return  {
+            'name': _('Grupos de pagos'),
+            'res_model': 'account.payment.group',
+            'view_mode': 'list,form',
+            'context': self.env.context,
+            'target': 'current',
+            'domain': [('id','in',self.payment_group_ids.ids)],
+            'type': 'ir.actions.act_window',
+        }
+    
+    def action_register_payment(self):
+        ctx = self.env.context.copy()
+        active_ids = self.ids
+        if not active_ids:
+            raise UserError('Por alguna razon el emisor/receptor del pago no pudo ser determinado')
+        invoices = self.env['account.move'].browse(active_ids).filtered(lambda move: move.is_invoice(include_receipts=True))
+        if any([invoice.move_type == 'in_invoice' for invoice in invoices]):
+            return super(AccountMove, self).action_register_payment()
+        ctx.update({
+            'default_partner_id':invoices[0].commercial_partner_id.id,
+            'active_ids':False,
+            'active_id':False,
+            'active_model':False,
+        })
+        
+        action = self.env.ref('account_payments_group.payments_group_window').sudo().read()[0]
+        action.update({
+            'view_mode': 'form',
+            'context': ctx,
+            'views':[(False,'form'),(False,'tree')]
+        })
+        return action
+
+    @depends('state','invoice_payments_widget')
+    def _compute_payment_group_ids(self):
+        for rec in self:
+            payment_groups = self.env['account.payment.group'].search([('move_line_ids','in',rec.line_ids.ids)]).ids
+            rec.payment_group_ids = payment_groups
+            rec.payment_group_count = len(payment_groups)
+
