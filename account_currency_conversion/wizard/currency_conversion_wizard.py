694872f78864ccc546ba57cb00a7c2f45a097351
Federico Gobea <65693362+fedegobea@users.noreply.github.com>
2022-11-07 12:01:39 -0300

    🚚 ccurrency_converson: splitted code for organizatoin

--- account_currency_conversion/wizard/currency_conversion_wizard.py
+++ account_currency_conversion/wizard/currency_conversion_wizard.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo.models import Model, TransientModel, AbstractModel
-from odoo.fields import Char,Float,Many2one,Date
+from odoo.models import TransientModel, AbstractModel
+from odoo.fields import Float, Many2one, Date
 from odoo.exceptions import UserError
 from odoo.api import onchange
 from odoo.tools import float_is_zero
@@ -110,47 +110,3 @@ class CurrencyConversionWizard(TransientModel):
       self._change_currency_rate_ids()
 
     
-class InvoiceCurrencyConversion(TransientModel):
-  _name="res.currency.conversion.invoice.wizard"
-  _description = "Conversion de moneda en factura"
-  _inherit = "res.currency.conversion.wizard"
-  
-  def confirm(self):
-    self.ensure_one()
-    ctx = self._context
-    active_ids = ctx.get('active_ids')
-    account_moves = self.env['account.move'].browse(active_ids).with_context(check_move_validity=False)
-    for am in account_moves:
-      if am.state == 'posted':
-        raise UserError(_('You cant change an already posted invoice'))
-      message = _("Currency changed from %s to %s with rate %s") % (am.currency_id.name, self.target_currency.name, self.exchange_rate)
-      for aml in am.line_ids: 
-        aml.write({
-            'price_unit': Float.round(aml.price_unit * self.exchange_rate, precision_rounding=self.target_currency.rounding),
-            'currency_id':self.target_currency.id
-        })
-        aml._onchange_mark_recompute_taxes()
-        aml._onchange_currency()
-      am.currency_id = self.target_currency.id
-      am.message_post(body=message)
-      super(InvoiceCurrencyConversion,self).confirm()
-    return {'type': 'ir.actions.act_window_close'}
-
-class PaymentCurrencyConversion(TransientModel):
-  _name="res.currency.conversion.payment.wizard"
-  _description = "Conversion de moneda en pago"
-  _inherit = "res.currency.conversion.wizard"
-  
-  def confirm(self):
-    self.ensure_one()
-    ctx = self._context
-    active_ids = ctx.get('active_ids')
-    super(PaymentCurrencyConversion,self).confirm()
-    payments = self.env['account.payment'].browse(active_ids)
-    # for payment in payments:
-    #   if payment.state in ['posted','sent','reconciled']:
-    #     raise UserError(_('You cant change an already posted invoice'))
-    
-    return {'type': 'ir.actions.act_window_close'}
-
-
---
+++ account_currency_conversion/wizard/invoice_currency_conversion_wizard.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from odoo.models import TransientModel
+from odoo.fields import Float
+from odoo.exceptions import UserError
+from odoo import _
+import logging
+
+class InvoiceCurrencyConversion(TransientModel):
+  _name="res.currency.conversion.invoice.wizard"
+  _description = "Conversion de moneda en factura"
+  _inherit = "res.currency.conversion.wizard"
+  
+  def confirm(self):
+    self.ensure_one()
+    ctx = self._context
+    active_ids = ctx.get('active_ids')
+    account_moves = self.env['account.move'].browse(active_ids).with_context(check_move_validity=False)
+    for am in account_moves:
+      if am.state == 'posted':
+        raise UserError(_('You cant change an already posted invoice'))
+      message = _("Currency changed from %s to %s with rate %s") % (am.currency_id.name, self.target_currency.name, self.exchange_rate)
+      for aml in am.line_ids: 
+        aml.write({
+            'price_unit': Float.round(aml.price_unit * self.exchange_rate, precision_rounding=self.target_currency.rounding),
+            'currency_id':self.target_currency.id
+        })
+        aml._onchange_mark_recompute_taxes()
+        aml._onchange_currency()
+      am.currency_id = self.target_currency.id
+      am.message_post(body=message)
+      super(InvoiceCurrencyConversion,self).confirm()
+    return {'type': 'ir.actions.act_window_close'}
