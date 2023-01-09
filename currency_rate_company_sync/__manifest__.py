869cae48544da67da06a74a69f7f6ccfb02f0c5d
Matias Rivera <mrivera@making.com.ar>
2023-01-09 10:35:34 -0300

    ✨ Now the rate sync between companies

---
+++ currency_rate_company_sync/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sincronizar Tipos de cambio',
+    'summary': 'Sincroniza los tipos de cambio en multi empresa',
+    'version': '1.0.1',
+    'author': 'Making Argentina',
+    'website': 'https://www.making.com.ar',
+    "category": "Accounting",
+    "depends": ['base'],
+    'data': [
+ 
+    ],
+    'installable': True,
+    'license': 'GPL-3',
+}
---
+++ currency_rate_company_sync/models/res_currency_rate.py
@@ -0,0 +1,48 @@
+from odoo.fields import  Date
+from odoo.models import Model
+from odoo import api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class res_currency_rate(Model):
+    _inherit = 'res.currency.rate'
+
+
+    @api.model
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        records.update_the_other_company()
+        return records
+
+
+    def update_the_other_company(self):
+        this_company = self.company_id
+        all_companies = self.env['res.company'].sudo().search([('currency_id','=', this_company.currency_id.id),('id', '!=', this_company.id)])
+       
+        for company in all_companies:
+            last_other_company_currency_rate = company.env['res.currency.rate'].sudo().search([('name', '=', Date.today()),('company_id', 'in', company.ids)])
+            if not last_other_company_currency_rate:
+                self.sudo().create([{
+                    'name': self.name,
+                    'rate': self.rate,
+                    'currency_id' : self.currency_id.id,
+                    'company_id' : company.id
+                }])
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #Currency = self.env['res.currency']
+        #CurrencyRate = self.env['res.currency.rate']
+#
+        #active_currencies = self.env['res.currency'].search([])
