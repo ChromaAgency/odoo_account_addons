304fa236727b556d3c937e45d2c721a9c51e93d8
MRivera <mrivera@making.com.ar>
2023-07-25 11:18:27 -0300

    ✨ RMA currency change update

--- account_rma_currency_invoicing/models/repair_order.py
+++ account_rma_currency_invoicing/models/repair_order.py
@@ -2,9 +2,8 @@ from odoo.exceptions import UserError
 from odoo import api, fields, models, _
 from odoo.fields import Date, Many2one
 from odoo.api import onchange
-import logging
-_logger=logging.getLogger(__name__)
-class product_pricelist_report(models.Model):
+
+class Repair(models.Model):
     _inherit = "repair.order"
     
     # TODO add to view
@@ -17,33 +16,19 @@ class product_pricelist_report(models.Model):
             rec.invoicing_currency = rec.partner_id.invoicing_currency
 
 
-    def _prepare_invoice(self):
-        """
-        Prepare the dict of values to create the new invoice for a sales order. This method may be
-        overridden to implement custom invoice generation (making sure to call super() to establish
-        a clean extension chain).
-        """
-        self.ensure_one()
-        invoice_vals = super()._prepare_invoice()
-        invoice_vals.update({
-            'currency_id': self.invoicing_currency.id,
+    def _create_invoices(self, group=False):
+        repair_invoice_dict = super()._create_invoices(group=group)
+        for repair, invoice in repair_invoice_dict.items():
+            account_move = self.env['account.move'].browse([invoice])
+            account_move.write({
+        'currency_id': self.invoicing_currency.id
         })
-        return invoice_vals
+            for line in account_move.invoice_line_ids:
+                line.write({
+                    'currency_id': self.invoicing_currency.id,
+                    'price_unit' : self.currency_id._convert(line.price_unit, self.invoicing_currency, self.env.company, Date.today()),
+                    'debit':  self.currency_id._convert(line.debit, self.invoicing_currency, self.env.company, Date.today()),
+                    'credit': self.currency_id._convert(line.credit, self.invoicing_currency, self.env.company, Date.today())
+                })
+        return repair_invoice_dict
 
-# class RepairOrderLine(models.Model):
-#     _inherit = 'repair.order.line'
-
-#     def _prepare_invoice_line(self, **optional_values):
-#         """
-#         Prepare the dict of values to create the new invoice line for a sales order line.
-
-#         :param qty: float quantity to invoice
-#         :param optional_values: any parameter that should be added to the returned invoice line
-#         """
-#         self.ensure_one()
-#         res = super()._prepare_invoice_line(**optional_values)
-#         if self.order_id.currency_id.id!=self.order_id.invoicing_currency.id:
-#             res.update({
-#                 'price_unit': self.order_id.currency_id._convert(self.price_unit, self.order_id.invoicing_currency, self.env.company, Date.today())
-#             })
-#         return res
