7252945bd67203850050b9a0c043a7735044f928
Federico Gobea <gitfgobea@gmail.com>
2023-08-23 10:45:51 -0500

    ✨ Add new module to avoid bad breahviour on check module

---
+++ account_check_printing_fix/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Fix account check printing",
+
+    'summary': """
+        Modulo para evitar los issues que tiene el modulo de cheques
+    """,
+
+    'description': """    """,
+
+    'author': "Making Argentina",
+    'website': "https://making.com.ar",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Account',
+    'version': '0.1',
+    'installable': True,
+    # any module necessary for this one to work correctly
+    'depends': ['account_check_printing'],
+    # always loaded
+    'data': [
+    
+    ]
+}
---
+++ account_check_printing_fix/models/account_payment.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models, _
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+    
+    @api.constrains('check_number', 'journal_id')
+    def _constrains_check_number(self):
+        checks = self.filtered(lambda x: x.payment_method_line_id.code == 'check_printing')
+        return super(self - checks).check_number_constrains()
