507b8447b8bb3c41229bf34446ee66e82269f586
MRIVERA <mrivera@making.com.ar>
2024-05-07 11:38:56 -0300

    ✨ Now we can filter only the moves

--- accounting_advanced_security/models/account_journal.py
+++ accounting_advanced_security/models/account_journal.py
@@ -5,3 +5,4 @@ class AccountJournal(models.Model):
     _inherit = "account.journal"
 
     group_ids = Many2many('res.groups', 'account_journal_group_rel', 'journal_id', 'group_id', string='Groups')
+    group_ids_only_hide_moves = Many2many('res.groups', 'account_journal_group_rel_only_hide_moves', 'journal_id', 'group_id', string='Groups only hide moves')
