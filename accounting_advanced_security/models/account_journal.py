ec87750b0105f8b303060966988d2ec29ac7a8d4
MRIVERA <mrivera@making.com.ar>
2024-05-08 17:39:06 -0300

    🐛 now it works well and looks fine

--- accounting_advanced_security/models/account_journal.py
+++ accounting_advanced_security/models/account_journal.py
@@ -5,5 +5,4 @@ class AccountJournal(models.Model):
     _inherit = "account.journal"
 
     group_ids = Many2many('res.groups', 'account_journal_group_rel', 'journal_id', 'group_id', string='Groups')
-    block_journal_visibility = Boolean(string="Bloquear la visibilidad de este diario")
     show_moves_only_to = Many2many('res.groups', 'account_journal_group_rel_hide_moves', 'journal_id', 'group_id', string='Mostrar movimientos solo a')
--- accounting_advanced_security/models/account_statement_line.py
+++ accounting_advanced_security/models/account_statement_line.py
@@ -10,10 +10,12 @@ class AccountBankStatementLine(models.Model):
     _inherit = 'account.bank.statement.line'
 
 
+
     def _action_open_bank_reconciliation_widget(self, extra_domain=None, default_context=None, name=None, kanban_first=True):
         action = super(AccountBankStatementLine, self)._action_open_bank_reconciliation_widget(extra_domain=extra_domain, default_context=default_context, name=name, kanban_first=kanban_first)
         journal = self.env['account.journal'].browse(action['context']['default_journal_id'])
-        if journal.block_journal_visibility:
-            if self.env.user.has_group('accounting_advanced_security.cannot_read_journal'):                
+        for group in journal.show_moves_only_to:
+            group_xmlid = group.get_metadata()[0].get('xmlid')
+            if not self.env.user.has_group(group_xmlid):
                 raise UserError(_('No tiene permisos para abrir este diario'))
         return action
